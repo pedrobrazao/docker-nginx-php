30e281cc072b87bb9889ad28ddb8a92d39355b89
Pedro Ferreira <pedro@thingston.com>
2026-02-03 18:22:53 +0000

    Enable HTTPS support

--- etc/docker/php-fpm.Dockerfile
+++
@@ -1,9 +0,0 @@
-FROM php:8-fpm
-
-
-RUN apt-get update && apt-get install -y \
-    zlib1g-dev \
-    libzip-dev \
-    zip \
-    unzip
-RUN docker-php-ext-install zip
---
+++ public/index.php
@@ -0,0 +1,17 @@
+    <?php
+
+use App\Factory\ApplicationFactory;
+use App\Factory\ContainerFactory;
+use Dotenv\Dotenv;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+Dotenv::createImmutable(__DIR__ . '/..')->safeLoad();
+
+new ApplicationFactory(
+    new ContainerFactory(
+        file_exists(__DIR__ . '/../config/container.php')
+        ? include __DIR__ . '/../config/container.php'
+        : include __DIR__ . '/../config/container.dist.php',
+    )->create(),
+)->create()->run();
